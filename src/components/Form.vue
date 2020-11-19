643f6631aa6d31584ee3e3fd43698c79d172ea73
jdavid2019 <joseph2019d@gmail.com>
2020-11-19 15:24:40 -0500

    validacionvue v1

--- src/components/Form.vue
+++ src/components/Form.vue
@@ -1,22 +1,28 @@
 <template>
   <div class="container mt-4">
     <h2>Validaciones con Vuejs</h2>
-    <form action="">
+    <!--directiva v-on || gestor de eventos || activamos ese evento que cuando se haga submit recoja el evento-->
+    <form name="form" id="form" v-on:submit.prevent="procesar();">
       <p>
         <label>Nombre :</label>
         <input
           type="text"
           class="form-control mb-4"
           name="nombre"
+          v-model="contacto.nombre"
           placeholder="Escribir un nombre"
+          autocomplete="off"
         />
       </p>
+        <div v-if="submited && !$v.contacto.nombre.required" class="alert alert-danger">El campo nombre es obligatorio</div>
+          <div v-if="submited && !$v.contacto.nombre.minLength" class="alert alert-danger">El campo nombre debe tener al menos 2 caracteres</div>
       <p>
         <label>Correo :</label>
         <input
           type="text"
           class="form-control"
           name="correo"
+          v-model="contacto.correo"
           placeholder="Escribir un correo"
         />
       </p>
@@ -26,24 +32,72 @@
           type="text"
           class="form-control"
           name="telefono"
+          v-model="contacto.telefono"
           placeholder="Escribir un teléfono"
         />
       </p>
 
-       <p>
+      <p>
         <label>Mensaje :</label>
-       <textarea name="mensaje" id="mensaje" cols="30" rows="10" class="form-control" placeholder="Escribe un mensaje ..."></textarea>
+        <textarea
+          name="mensaje"
+          id="mensaje"
+          cols="30"
+          rows="10"
+          v-model="contacto.mensaje"
+          class="form-control"
+          placeholder="Escribe un mensaje ..."
+        ></textarea>
       </p>
-      <hr>
-      <input type="submit" value="Enviar datos" class="btn btn-success">
+      <hr />
+      <input type="submit" value="Enviar datos" class="btn btn-success" />
     </form>
   </div>
 </template>
 
 <script>
+//2
+import { required, minLength, email } from 'vuelidate/lib/validators';
+//ejecuta binding
 export default {
   name: "Form",
-};
+  data() {
+    return {
+      //1. creo mi objeto y lo asocio a mis inputs , eso es hacer binding
+      submited: false,
+      contacto: {
+        nombre: '',
+        correo: '',
+        telefono: '',
+        mensaje: ''
+      },
+    };
+  },
+  methods: {
+    procesar() {
+        this.submited = true;
+        this.$v.$touch();
+      if (this.$v.$invalid) {
+        return false;
+      }
+      alert(this.contacto.nombre);
+    }
+  },
+  //3
+  validations: {
+    contacto: {
+      nombre: {
+        required,
+        minLength: minLength(2)
+      },
+      correo: {
+        required,
+        minLength: minLength(2),
+        email
+      }
+    }
+  }
+}
 </script>
 
 <style>
--- src/main.js
+++ src/main.js
@@ -1,11 +1,12 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Vuelidate from 'vuelidate';
 import 'jquery';
 import 'popper.js';
 import 'bootstrap';
 import './assets/app.css';
 Vue.config.productionTip = false
-
+Vue.use(Vuelidate);
 new Vue({
   render: h => h(App),
 }).$mount('#app')
